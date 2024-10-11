bc189ddd0e7c806abbc26a70d87832289a6054ba
Damien Palm <palm.damien.pro@gmail.com>
2024-10-11 12:37:35 +0200

    Modification du fichier Header.js
    Pour le rendre plus React-like

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,32 +3,24 @@ class Header {
     this.logoSrc = "../src/assets/images/logo/logo.png";
     this.title = "Les petits plats";
   }
-  createHeaderDOM() {
-    const header = document.createElement("header");
-    header.classList.add("header");
 
-    const headerContent = `
-    <a href="./index.html" class="header__logo-link">
-        <img src="${this.logoSrc}" alt="${this.title}" class="header__logo-link__logo">
-    </a>
-    <h1 class="header__main-title">Cherchez parmi plus de 1500 recettes du quotidien, simples et délicieuses</h1>
-    <form action="" method="get" class="header__search-bar-form">
-        <label for="header__search-bar"></label>
-        <input type="text" name="search-bar" id="header__search-bar" placeholder="Recherchez une recette, un ingrédient, ...">
-        <button class="header__search-bar-form__button" type="submit">
+  render() {
+    return `
+    <header class="header">
+        <a href="./index.html" class="header__logo-link">
+          <img src="${this.logoSrc}" alt="${this.title}" class="header__logo-link__logo">
+        </a>
+        <h1 class="header__main-title">Cherchez parmi plus de 1500 recettes du quotidien, simples et délicieuses</h1>
+        <form action="" method="get" class="header__search-bar-form">
+          <label for="header__search-bar"></label>
+          <input type="text" name="search-bar" id="header__search-bar" placeholder="Recherchez une recette, un ingrédient, ...">
+          <button class="header__search-bar-form__button" type="submit">
             <i class="fa-solid fa-magnifying-glass header__search-bar-form__button__button-icon"></i>
-        </button>
-        <div class="header__search-bar-suggestions"></div>
-    </form>
+          </button>
+          <div class="header__search-bar-suggestions"></div>
+        </form>
+    </header>
     `;
-
-    header.innerHTML = headerContent;
-    return header;
-  }
-
-  render(parentElement) {
-    const headerElement = this.createHeaderDOM();
-    parentElement.appendChild(headerElement);
   }
 }
 
